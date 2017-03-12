0181864a6954a50d6c2b2a16453ec8fad8a11f31
Will McGinnis <will@predikto.com>
2017-03-12 11:42:16 -0400

    Added access date to Webpage, and moved to using properties.

--- rasp/base.py
+++ rasp/base.py
@@ -1,7 +1,7 @@
 import urllib
 import urllib.error
 import urllib.request
-
+import time
 from rasp.constants import DEFAULT_USER_AGENT
 from rasp.errors import EngineError
 
@@ -37,14 +37,37 @@ class DefaultEngine(Engine):
 
 class Webpage(object):
     def __init__(self, url=None, source=None):
-        self.url = url
-        self.source = source
+        """The Webpage object represents all we know about a single scraped page.
 
-    def set_source(self, source):
-        self.source = source
+        The Webpage object is the key object constructed by an engine to represent what we know about a given webpage.
+        It includes things like the page source, url, and date of access.
 
-    def set_url(self, url):
-        self.url = url
+        :param url: the url of the webpage you are representing
+        :param source: the source, as text of the webpage.
+        :return: Webpage
+        """
+
+        self._url = url
+        self._source = source
+        self._access_date = time.time()
+
+    @property
+    def source(self):
+        """Source of the webpage, in text.
+        """
+        return self._source
+
+    @property
+    def url(self):
+        """Url of the webpage accessed
+        """
+        return self._url
+
+    @property
+    def access_date(self):
+        """Date of access of the webpage data, as a unix timestamp in UTC
+        """
+        return self._access_date
 
     def __repr__(self):
-        return "url: {}".format(self.url)
+        return "url: {} at {}".format(self.url, self.access_date)
