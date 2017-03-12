6d70c62335cebaa469c23417ad31cf36af2cf0f3
Harrison Bond <baronvonbadguy@gmail.com>
2017-03-12 15:02:21 -0400

    Add docs for DefaultEngine and TorEngine classes

--- rasp/base.py
+++ rasp/base.py
@@ -15,6 +15,13 @@ class Engine(object):
 
 
 class DefaultEngine(Engine):
+    """The parent class for all ``requests`` based engines.
+
+    Attributes:
+        session (:obj:`requests.Session`): Session object for which all
+            requests are routed through.
+        headers (dict): Base headers for all requests.
+    """
     def __init__(self, headers=None):
         self.session = self._session()
         self.headers = headers or {'User-Agent': DEFAULT_USER_AGENT}
@@ -24,9 +31,31 @@ class DefaultEngine(Engine):
         return DefaultEngine(self.headers)
 
     def _session(self, *args, **kwargs):
+        """Internal Session object creator.
+
+        Note:
+            This method exists to accommodate injecting a
+            mock Session object during testing runtime.
+
+        Returns:
+            ``requests.Session``
+        """
         return requests.session(*args, **kwargs)
 
     def get_page_source(self, url, params=None, headers=None):
+        """Fetches the specified url.
+
+        Attributes:
+            url (str): The url of which to fetch the page source code.
+            params (dict, optional): Key\:Value pairs to be converted to
+                x-www-form-urlencoded url parameters_.
+            headers (dict, optional): Extra headers to be merged into
+                base headers for current Engine before requesting url.
+        Returns:
+                    ``rasp.base.Webpage`` if successful, ``None`` if not
+
+        .. _parameters: http://docs.python-requests.org/en/master/user/quickstart/#passing-parameters-in-urls
+        """
         if not url:
             raise ValueError('url needs to be specified')
         if isinstance(headers, dict):
--- rasp/tor_engine.py
+++ rasp/tor_engine.py
@@ -8,6 +8,31 @@ from rasp.base import DefaultEngine
 
 
 class TorEngine(DefaultEngine):
+    """Uses ``Tor`` as a socks proxy to route all ``requests`` based calls through.
+
+    This engine provides two functions on top of ``DefaultEngine``:
+        1. Route web requests through an anonymous proxy.
+        2. Get new endpoint IP address for every request.
+
+    Attributes:
+        session (:obj:`requests.Session`): Session object for which all
+         requests are routed through.
+        headers (dict, optional): Base headers for all requests.
+        address (str): IP address of Tor SOCKS proxy to connect,
+            can also be set with the ``RASP_TOR_ADDRESS``
+            environment variable.
+        port (int): Port number of Tor SOCKS proxy for web requests,
+            can also be set with the ``RASP_TOR_PORT``
+            environment variable.
+        control_port (int, optional): Port number for control
+            port usage to refresh endpoint address,
+            can also be set with the ``RASP_TOR_CONTROL_PASSWORD``
+            environment variable.
+        control_password (str, optional): Password to protect control
+            port usage,
+            can also be set with the ``RASP_TOR_CONTROL_PORT``
+            environment variable.
+     """
     def __init__(self,
                  headers=None,
                  address=None,
@@ -52,6 +77,11 @@ class TorEngine(DefaultEngine):
         )
 
     def clean_circuits(self):
+        """Refreshes the mapping of nodes we connect through.
+
+        When a new path from the user to the destination is required, this
+        method refreshes the node path we use to connect. This gives us a new IP
+        address with which the destination sees."""
         kwargs = {'address': self.address, 'port': self.control_port}
         with Controller.from_port(**kwargs) as controller:
             controller.authenticate(password=self.control_password)
@@ -59,6 +89,21 @@ class TorEngine(DefaultEngine):
 
     def get_page_source(self, url, params=None,
                         headers=None, clean_circuits=True):
+        """Fetches the specified url.
+
+        Attributes:
+            url (str): The url of which to fetch the page source code.
+            params (dict, optional): Key\:Value pairs to be converted to
+                x-www-form-urlencoded url parameters_.
+            headers (dict, optional): Extra headers to be merged into
+                base headers for current Engine before requesting url.
+            clean_circuits (bool, optional): Determines whether or not
+                we want to get a new IP with which to connect ``url``.
+        Returns:
+            ``rasp.base.Webpage`` if successful, ``None`` if not
+
+        .. _parameters: http://docs.python-requests.org/en/master/user/quickstart/#passing-parameters-in-urls
+        """
         if clean_circuits:
             self.clean_circuits()
         return super(TorEngine, self).get_page_source(url, params=params, headers=headers)
